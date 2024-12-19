4dbbc4dae1bb079b2cdee2206496f489321e1657
SU-T <guitaristpeet@gmail.com>
2024-12-18 21:41:02 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
+
+const apiClient = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${accessToken}`,
+  },
+});
+
+const fetchData = async (method, endpoint, params = {}) => {
+  try {
+    const response = await apiClient.request({
+      method,
+      url: endpoint,
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+let imgUrl = "";
+let imgSize = "";
+
+const fetchConfig = fetchData("GET", "/configuration")
+  .then((data) => {
+    // console.log(data.images);
+    const imgUrl = data.images.secure_base_url;
+    const sizes = data.images.poster_sizes;
+    const imgSize = sizes[3];
+  })
+  .catch((error) => console.log(error));
+
+const fetchPopularTv = fetchData("GET", "/tv/popular", {
+  language: "en-US",
+  page: "1",
+})
+  .then((data) => {
+    const results = data.results;
+    results.forEach((result) => {
+      const imgPath = result.poster_path;
+    });
+  })
+  .catch((error) => console.log(error));
