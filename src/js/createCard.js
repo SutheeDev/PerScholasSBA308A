51ce44cf2ea684eb6b3e0ed183c51423b4be6fd4
SU-T <guitaristpeet@gmail.com>
2024-12-31 17:50:10 -0500

    styled cards

--- src/js/createCard.js
+++ src/js/createCard.js
@@ -4,6 +4,8 @@ const createCard = (name, imgUrl, id) => {
   // Create elements
   const card = document.createElement("div");
   const cardFrame = document.createElement("div");
+  const cardContent = document.createElement("div");
+  const cardTextContent = document.createElement("div");
   const cardImgContainer = document.createElement("div");
   const cardImg = document.createElement("img");
   const wishlist = document.createElement("div");
@@ -13,16 +15,26 @@ const createCard = (name, imgUrl, id) => {
   // Add classes
   card.classList.add("card");
   cardFrame.classList.add("cardFrame");
+  cardContent.classList.add("card-content");
+  cardTextContent.classList.add("card-text-content");
   cardImg.classList.add("cardImg");
   cardImgContainer.classList.add("card-img-container");
   wishlist.classList.add("wishlist");
+  title.classList.add("title");
 
   // Add attribute
   cardImg.setAttribute("src", imgUrl);
 
+  //   Add content
+  title.innerText = name;
+
   // Assemble
+  cardTextContent.appendChild(title);
   cardImgContainer.appendChild(cardImg);
-  card.appendChild(cardImgContainer);
+  cardImgContainer.appendChild(wishlist);
+  cardContent.appendChild(cardImgContainer);
+  cardContent.appendChild(cardTextContent);
+  card.appendChild(cardContent);
   card.appendChild(cardFrame);
 
   cardsContainer.appendChild(card);
